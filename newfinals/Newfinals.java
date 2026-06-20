5f27574b66e7fbd88e352465327b6ebc123470b1
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-06-21 01:28:33 +0530

    final method

---
+++ newfinals/Newfinals.java
@@ -0,0 +1,19 @@
+package newfinals;
+
+class Parent {
+    final void show() {
+        System.out.println("Parent method");
+    }
+}
+
+class Child extends Parent {
+    void show() {
+        System.out.println("Child method");
+    }
+}
+
+public class Newfinals {
+    public static void main(String[] args) {
+        
+    }
+}
