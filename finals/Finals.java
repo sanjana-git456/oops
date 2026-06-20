ddbdedb3360438a20f4632734fd5425fd84f4869
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-06-21 01:24:50 +0530

    final variable

---
+++ finals/Finals.java
@@ -0,0 +1,9 @@
+package finals;
+
+public class Finals {
+    public static void main(String[] args) {
+        final float pi = 3.14f;
+        // pi = 5;
+        System.out.println(pi);
+    }
+}
